909ad4ed7a5d0f7e3943e907b0d71932522908bb
kynmh69 <hiroki.kayanuma@gmail.com>
2020-04-11 15:31:44 +0900

    create df

--- src/analyze_covid_19.py
+++ src/analyze_covid_19.py
@@ -1,5 +1,18 @@
 import requests
+import json
+from typing import Union, Dict, List, Any
+from pandas import DataFrame, isnull
+from datetime import datetime
 
-uri =  'https://services6.arcgis.com/5jNaHNYe2AnnqRnS/arcgis/rest/services/COVID19_JapanData/FeatureServer/0/query?where=%E9%80%9A%E3%81%97%3E0&returnIdsOnly=false&returnCountOnly=false&&f=pgeojson&outFields=*&orderByFields=%E9%80%9A%E3%81%97'
-
+uri =  ('https://services6.arcgis.com/5jNaHNYe2AnnqRnS/arcgis/rest/services/COVID19_JapanData/FeatureServer/0'
+        '/query?where=%E9%80%9A%E3%81%97%3E0&returnIdsOnly=false&returnCountOnly=false&&f='
+        'pgeojson&outFields=*&orderByFields=%E9%80%9A%E3%81%97')
+encoding = 'utf-8'
 res = requests.get(uri)
+print(res.json()["features"][0]["properties"])
+print(json.dumps(res.json()["features"][0]["properties"], indent=4, ensure_ascii=False))
+propertyies_data = []
+for prop in res.json()["features"]:
+    propertyies_data.append(prop["properties"])
+
+df = DataFrame(propertyies_data)
