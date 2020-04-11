a4b73d05489d818a05e82f076fd9fe309f583d7b
kynmh69 <hiroki.kayanuma@gmail.com>
2020-04-11 13:30:07 +0900

    initial commt

---
+++ src/analyze_covid_19.py
@@ -0,0 +1,4 @@
+import requests
+
+uri =  'https://services6.arcgis.com/5jNaHNYe2AnnqRnS/arcgis/rest/services/COVID19_JapanData/FeatureServer/0/query?where=%E9%80%9A%E3%81%97%3E0&returnIdsOnly=false&returnCountOnly=false&&f=pgeojson&outFields=*&orderByFields=%E9%80%9A%E3%81%97'
+
